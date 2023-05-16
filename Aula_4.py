892874bcd491483f6b1c2943a8471da17813d023
omartins-zs <gabrielmatheus209@gmail.com>
2023-05-16 08:44:20 -0300

    Iniciando a Raspagem dos Dados na Aula 4

---
+++ Aula_4.py
@@ -0,0 +1,8 @@
+# Iniciando a Raspagem de Dados
+
+# Com o WebDrive a gente consegue a pedir a página (URL)
+wd_Chrome.get("https://www.flashscore.com/")
+
+## Para jogos do dia seguinte
+#wd_Chrome.find_element(By.CSS_SELECTOR,'div.calendar__navigation--tomorrow').click()
+#time.sleep(2)
